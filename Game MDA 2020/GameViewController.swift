d2188db091dd0f3738de227e17003bf90ea312eb
Denis Bystruev <dbystruev@me.com>
2020-11-27 16:59:36 +0300

    Fixed clone ship issue

--- Game MDA 2020/GameViewController.swift
+++ Game MDA 2020/GameViewController.swift
@@ -15,6 +15,7 @@ class GameViewController: UIViewController {
     let button = UIButton()
     
     // MARK: - Stored Properties
+    var ship: SCNNode!
     var scene: SCNScene!
     var scnView: SCNView!
     
@@ -43,7 +44,7 @@ class GameViewController: UIViewController {
     /// - Returns: SCNNode with the new ship
     func getShip() -> SCNNode {
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        ship = scene.rootNode.childNode(withName: "ship", recursively: true)!.clone()
         
         // Move ship far away
         let x = 25
@@ -54,14 +55,13 @@ class GameViewController: UIViewController {
         
         // Add animation to move the ship to origin
         ship.runAction(.move(to: SCNVector3(), duration: 5)) {
-            print(#line, #function)
-            ship.removeFromParentNode()
+            self.ship.removeFromParentNode()
             DispatchQueue.main.async {
                 self.button.isHidden = false
             }
         }
         
-        return ship.clone()
+        return ship
     }
     
     /// Finds and removes the ship from the scene
